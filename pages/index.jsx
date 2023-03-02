709458b95b586e5bce6cbd4ebe146d5f91d1e5ea
Matt Smith <matt.smith3108@gmail.com>
2023-03-02 12:38:29 +0000

    Added prettier and prettier tailwindcss

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,12 +6,18 @@ export default function Home() {
     <>
       <Head>
         <title>Splash</title>
-        <meta name="description" content="You can't drink wine when your mouth is full of objectives." />
+        <meta
+          name="description"
+          content="You can't drink wine when your mouth is full of objectives."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1 className='text-xl'>Hi there</h1>
+        <h1 className="text-xl">Hi there</h1>
+        <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+          hi
+        </button>
       </main>
     </>
   )
