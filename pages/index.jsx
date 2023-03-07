55da68b1aa1caa33b7d947ad0cbe898bdfc73646
Matt Smith <matt.smith3108@gmail.com>
2023-03-07 16:24:07 +0000

    Added tailwind classes to index

--- pages/index.jsx
+++ pages/index.jsx
@@ -38,7 +38,7 @@ export default function Home() {
             <Form />
             <FallingWords constraintsRef={constraintsRef} />
           </motion.div>
-          <div className="left-[5%] left-[10%] left-[15%] left-[20%] left-[25%] left-[30%] left-[35%] left-[40%] left-[45%] left-[50%] left-[55%] left-[60%] left-[65%] left-[70%] left-[75%] left-[80%] left-[85%] left-[90%]"></div>
+          <div className="left-[5%] left-[10%] left-[15%] left-[20%] left-[25%] left-[30%] left-[35%] left-[40%] left-[45%] left-[50%] left-[55%] left-[60%] left-[65%] left-[70%] left-[75%] left-[80%] left-[85%] left-[90%] text-blue text-yellow"></div>
         </main>
       )}
     </AnimatePresence>
